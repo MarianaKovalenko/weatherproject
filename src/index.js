fb20a9a0a0c27b23eb28957b720b0b5f36d88f45
Mariana Kovalenko <kovalenko0209@gmail.com>
2023-02-22 16:44:50 +0200

    moved daily forecast elements to index.js

--- src/index.js
+++ src/index.js
@@ -222,3 +222,37 @@ function showCTemp() {
   currentTemp.innerHTML = 10;
 }
 cTemp.addEventListener("click", showCTemp);
+//displayForecast
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="col-sm-2" id="first-day">`;
+
+  let days = ["SAT", "SUN", "MON", "TUE", "WED", "THU"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            ${day}
+          <div class="col-sm">
+            <img id="icon" src="src/cloudy.gif" alt="snowflake" width="48px" />
+          </div>
+          <div class="temp">
+            <div class="col-sm">
+              min 
+            </br>
+            -2°C |°F
+            </div>
+            <div class="col-sm">
+              min 
+            </br>
+            -2°C |°F
+            </div>
+          </div>
+        `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+};
+
+displayForecast();
